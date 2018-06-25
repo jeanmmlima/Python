cc24c25b9293ddb48cced572e637d436c48eb2fa
Jean Mário <jean@dca.ufrn.br>
2018-06-25 11:34:11 -0300

    erro de conexao com a porta resolvido

--- Driver AL5D LYNX/ufrn_al5d.py
+++ Driver AL5D LYNX/ufrn_al5d.py
@@ -3,37 +3,61 @@
 """
 Created on Wed Jun 20 15:24:07 2018
 
+
 @author: jean
 """
 import time
 import serial
+import os
 
- 
+## $ sudo apt-get install python-serial
+
 SERIAL_PORT='/dev/ttyS0' #Serial
 BAUD_RATE='115200'       #Set Baud rates to 115200
 TIMEOUT='1'              #Timeout after opening
 PARITY='N'               # No parity
 STOPBITS='1'             
-BYTESIZE='8'        
+BYTESIZE='8'
+XONXOFF = False
+RTSCTS = False
 
-def abre():
-    print ('\nObtendo informacoes sobre a comunicacao serial\n')
-    # Iniciando conexao serial
-    #comport = serial.Serial(DEVICE, 115200, timeout=1)
-    serial_port = serial.Serial(SERIAL_PORT,
-        int(BAUD_RATE),
-        timeout=int(TIMEOUT),
-        bytesize=int(BYTESIZE),
-        stopbits=int(STOPBITS),
-        parity=PARITY)
+#estabelece serial ttyS0
+
+os.system('sudo chmod 777 /dev/ttyS0')
+os.system('sudo rm /dev/ttyS0')  
+os.system('sudo ln -s /dev/ttyUSB0 /dev/ttyS0')      
+
+#configura porta
+#serial_port = serial.Serial('/dev/ttyS0',115200)
+serial_port = serial.Serial(SERIAL_PORT, 
+int(BAUD_RATE), 
+timeout=int(TIMEOUT), 
+bytesize=int(BYTESIZE), 
+stopbits=int(STOPBITS), 
+parity=PARITY,
+xonxoff = XONXOFF,
+rtscts= RTSCTS)
+
+
+#abre porta
+if not serial_port.is_open:
+    serial_port.open()
+
+print ('\nObtendo informacoes sobre a comunicacao serial\n')
  # Alem das opcoes rtscts=BOOL, xonxoff=BOOL, e dsrdtr=BOOL
-    time.sleep(1.8) # Entre 1.5s a 2s
-    print ('\nStatus Porta: %s ' % (serial_port.isOpen()))
-    print ('Device conectado: %s ' % (serial_port.name))
-    print ('Dump da configuracao:\n %s ' % (serial_port))
-    print('\n###############################################\n')
-    serial_port.close()
+time.sleep(1.8) # Entre 1.5s a 2s
 
-""" main """
-if __name__ == '__main__':
-    abre()
+#testa porta
+print ('\nStatus Porta: %s ' % (serial_port.isOpen()))
+print ('Device conectado: %s ' % (serial_port.name))
+print ('Dump da configuracao:\n %s ' % (serial_port))
+print('\n###############################################\n')
+print('HOME POSITION... \n')
+time.sleep(1.5) # Entre 1.5s a 2s
+serial_port.write('#0P1500#1P1500#2P1500#3P1500#4P1500T1500\r')
+time.sleep(1.5)
+serial_port.write('#0P1000T200\r')
+      
+#fecha porta      
+serial_port.close() 
+
